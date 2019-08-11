bf0c9e6964aecf46f10d7441e4a27b248c74040d
Yuns Xie <yuns.xie@qq.com>
2019-08-11 17:34:56 +0800

    chore: remove console addon of storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,4 @@
-import { configure, addDecorator } from '@storybook/react';
-import { setConsoleOptions, withConsole } from '@storybook/addon-console';
-
-setConsoleOptions({
-  panelExclude: [],
-});
-
-addDecorator((storyFn, context) => withConsole()(storyFn)(context));
+import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../stories', true, /\.stories\.tsx$/);
