9169d1c00be54a6a5946b1b0a0efe7580a9582f9
Yuns Xie <yuns.xie@qq.com>
2019-08-29 23:01:56 +0800

    feat: export specific config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-export {
+import {
   setQueryPanelText,
   setDetailFormDrawerText,
   setFormatSorter,
   setSearchFieldName,
 } from './config';
 
+export const config = {
+  setQueryPanelText,
+  setDetailFormDrawerText,
+  /*! only set how to format sorter, not set container's handleFilterAndSort */
+  setFormatSorter,
+  setSearchFieldName,
+}
+
 export { default as Curd } from './Curd';
-export { FormMate, config } from './FormMate';
+export { FormMate, config as FormMateConfig, locale as FormMateLocale } from './FormMate';
 
 export { default as StandardTable } from './components/StandardTable';
 export { default as StandardList } from './components/StandardList';
