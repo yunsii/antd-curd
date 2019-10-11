7e8c75cd617bf528f1717622973245f83eb7e475
Yuns Xie <yuns.xie@qq.com>
2019-10-11 14:30:12 +0800

    feat: isDevelopmentEnv

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const CreateName = 'create';
 export const DetailName = 'detail';
 export const UpdateName = 'update';
+
+export const isDevelopmentEnv = process.env.NODE_ENV === 'development';
