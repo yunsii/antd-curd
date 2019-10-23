0fc217c0f8bb3fa0cf5b6dead771bf14ba93b814
Yuns Xie <yuns.xie@qq.com>
2019-10-23 12:17:24 +0800

    chore: field rename

--- src/components/CurdBox/Operators/index.tsx
+++ src/components/CurdBox/Operators/index.tsx
@@ -5,17 +5,17 @@ import styles from './index.less';
 import CurdBox from '../index';
 
 export interface OperatorsProps {
-  curdBox: CurdBox;
+  __curdBox__: CurdBox<any>;
   createButtonName?: string | false;
   children?: React.ReactChildren;
-} 
+}
 
 export default function Operators(props: OperatorsProps) {
-  const { createButtonName, children, curdBox } = props;
+  const { createButtonName, children, __curdBox__ } = props;
   return (
     <div className={styles.tableListOperator}>
       {createButtonName ? (
-        <Button icon="plus" type="primary" onClick={() => curdBox.handleVisible(CreateName, true)}>
+        <Button icon="plus" type="primary" onClick={() => __curdBox__.handleVisible(CreateName, true)}>
           {createButtonName}
         </Button>
       ) : null}
