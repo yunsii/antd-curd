466ef1bcaa548aa599fe45c67110038d250b3408
Yuns Xie <yuns.xie@qq.com>
2019-10-28 23:14:46 +0800

    feat: remove children

--- src/components/CurdList/index.tsx
+++ src/components/CurdList/index.tsx
@@ -1,49 +1,44 @@
-import React, { useContext, Fragment } from 'react';
+import React, { useContext } from 'react';
 import StandardList, { StandardListProps } from '../StandardList/index';
 import CurdBox, { CurdBoxProps } from '../CurdBox';
-import { injectChildren } from '../../utils';
 import DataContext from '../../DataContext';
 
 type NoDataStandardTableProps<T> = Omit<StandardListProps<T>, 'data'>;
 
 export interface CustomStandardListProps<T> extends NoDataStandardTableProps<T> {
-	__curdBox__?: CurdBox;
-	fetchLoading?: boolean;
-	children?: React.ReactChildren;
+  __curdBox__?: CurdBox;
+  fetchLoading?: boolean;
 }
 
 function CustomStandardList<T>(props: CustomStandardListProps<T>) {
-	const { __curdBox__, fetchLoading, onSelectRow, rowKey, checkable, selectedRows, renderItem, children } = props;
+  const { __curdBox__, fetchLoading, onSelectRow, rowKey, checkable, selectedRows, renderItem } = props;
   const { data } = useContext(DataContext);
-	if (__curdBox__) {
-		const { handleDataChange } = __curdBox__;
-		return (
-			<Fragment>
-				<StandardList
-					loading={fetchLoading}
-					setActions={(record) => __curdBox__.renderActions(record)}
-					onChange={handleDataChange}
-					data={data}
-					onSelectRow={onSelectRow}
-					rowKey={rowKey}
-					checkable={checkable}
-					selectedRows={selectedRows}
-					renderItem={renderItem}
-				/>
-				{injectChildren(children, { __curdBox__ })}
-			</Fragment>
-		);
-	}
-	return null;
+  if (__curdBox__) {
+    const { handleDataChange } = __curdBox__;
+    return (
+      <StandardList
+        loading={fetchLoading}
+        setActions={(record) => __curdBox__.renderActions(record)}
+        onChange={handleDataChange}
+        data={data}
+        onSelectRow={onSelectRow}
+        rowKey={rowKey}
+        checkable={checkable}
+        selectedRows={selectedRows}
+        renderItem={renderItem}
+      />
+    );
+  }
+  return null;
 }
 
-export interface CurdListProps<T> extends CustomStandardListProps<T>, CurdBoxProps {}
+export interface CurdListProps<T> extends CustomStandardListProps<T>, CurdBoxProps { }
 
 export default function CurdList<T>(props: CurdListProps<T>) {
-	const { renderItem, ...rest } = props;
-	return (
-		<CurdBox {...rest}>
-			<CustomStandardList {...props} />
-		</CurdBox>
-	);
+  const { renderItem, ...rest } = props;
+  return (
+    <CurdBox {...rest}>
+      <CustomStandardList {...props} />
+    </CurdBox>
+  );
 }
--- src/components/CurdTable/index.tsx
+++ src/components/CurdTable/index.tsx
@@ -1,60 +1,55 @@
-import React, { useContext, Fragment } from 'react';
+import React, { useContext } from 'react';
 import StandardTable, { StandardTableProps, StandardTableColumnProps } from '../StandardTable/index';
 import CurdBox, { CurdBoxProps } from '../CurdBox';
 import { addDivider } from '../../utils';
-import { injectChildren } from '../../utils';
 import DataContext from '../../DataContext';
 
 type NoDataStandardTableProps<T> = Omit<StandardTableProps<T>, 'data'>;
 
 export interface CustomStandardTableProps<T> extends NoDataStandardTableProps<T> {
-	__curdBox__?: CurdBox;
-	columns: StandardTableColumnProps<T>[];
-	fetchLoading?: boolean;
-	children?: React.ReactChildren;
+  __curdBox__?: CurdBox;
+  columns: StandardTableColumnProps<T>[];
+  fetchLoading?: boolean;
 }
 
 function CustomStandardTable<T>(props: CustomStandardTableProps<T>) {
-	const { __curdBox__, columns, fetchLoading, children, ...rest } = props;
-	const { data } = useContext(DataContext);
-	if (__curdBox__) {
-		const { handleDataChange } = __curdBox__;
-		const enhanceColumns = () => {
-			const { actionsConfig } = __curdBox__.props;
-			if (!columns) return [];
-			if (!actionsConfig) return columns;
-			return [
-				...columns,
-				{
-					title: '操作',
-					render: (value, record) => {
-						return addDivider(__curdBox__.renderActions(record));
-					}
-				}
-			];
-		};
-		return (
-			<Fragment>
-				<StandardTable
-					{...rest}
-          data={data}
-					loading={fetchLoading}
-					columns={enhanceColumns()}
-					onChange={handleDataChange}
-				/>
-				{injectChildren(children, { __curdBox__ })}
-			</Fragment>
-		);
-	}
-	return null;
+  const { __curdBox__, columns, fetchLoading, ...rest } = props;
+  const { data } = useContext(DataContext);
+  if (__curdBox__) {
+    const { handleDataChange } = __curdBox__;
+    const enhanceColumns = () => {
+      const { actionsConfig } = __curdBox__.props;
+      if (!columns) return [];
+      if (!actionsConfig) return columns;
+      return [
+        ...columns,
+        {
+          title: '操作',
+          render: (value, record) => {
+            return addDivider(__curdBox__.renderActions(record));
+          }
+        }
+      ];
+    };
+    return (
+      <StandardTable
+        {...rest}
+        data={data}
+        loading={fetchLoading}
+        columns={enhanceColumns()}
+        onChange={handleDataChange}
+      />
+    );
+  }
+  return null;
 }
 
-export interface CurdTableProps<T> extends CustomStandardTableProps<T>, CurdBoxProps {}
+export interface CurdTableProps<T> extends CustomStandardTableProps<T>, CurdBoxProps { }
 
 export default function CurdTable<T>(props: CurdTableProps<T>) {
-	return (
-		<CurdBox {...props}>
-			<CustomStandardTable {...props} />
-		</CurdBox>
-	);
+  return (
+    <CurdBox {...props}>
+      <CustomStandardTable {...props} />
+    </CurdBox>
+  );
 }
