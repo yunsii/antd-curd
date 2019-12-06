0aee995b4b770454f24ca497a1a438fc0c9cf548
Yuns Xie <yuns.xie@qq.com>
2019-12-06 23:40:17 +0800

    refactor: export ConfigProvider and FormConfigProvider

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,7 @@
-import {
-  setQueryPanelText,
-  setDetailFormDrawerText,
-  setFormatSorter,
-  setSearchFieldName,
-  setDebounceWait,
-} from './config';
-
-export const config = {
-  setQueryPanelText,
-  setDetailFormDrawerText,
-  /** only set how to format sorter, not set container's handleFilterAndSort */
-  setFormatSorter,
-  setSearchFieldName,
-  setDebounceWait,
-}
-
+export { ConfigProvider } from './ConfigContext';
 export { default as Curd } from './Curd';
-export { createFormItems, config as formMateConfig, locale as formMateLocale } from './FormMate';
+
+export { createFormItems, ConfigProvider as FormConfigProvider } from './FormMate';
 
 export { default as StandardTable } from './components/StandardTable';
 export { default as StandardList } from './components/StandardList';
