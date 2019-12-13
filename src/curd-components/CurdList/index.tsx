a0578932c0568f758a09dd7e4d038ab5550c56d5
Yuns Xie <yuns.xie@qq.com>
2019-12-13 09:59:28 +0800

    refactor: remove actionsConfig

--- src/curd-components/CurdList/index.tsx
+++ src/curd-components/CurdList/index.tsx
@@ -1,22 +1,21 @@
 import React, { useContext } from 'react';
 import StandardList, { StandardListProps } from '../../components/StandardList/index';
-import CurdBox, { CurdBoxProps } from '../CurdBox';
+import CurdBox from '../CurdBox';
 import DataContext from '../../DataContext';
 
 type NoDataStandardTableProps<T> = Omit<StandardListProps<T>, 'data'>;
 
 export interface CustomStandardListProps<T extends { id: number | string }> extends NoDataStandardTableProps<T> {
-  actionsConfig?: CurdBoxProps<T>['actionsConfig'];
   renderActions?: CurdBox<T>['renderActions'];
   handleDataChange?: CurdBox<T>['handleDataChange'];
 }
 export default function CustomStandardList<T extends { id: number | string }>(props: CustomStandardListProps<T>) {
-  const { actionsConfig, handleDataChange, renderActions, ...rest } = props;
+  const { handleDataChange, renderActions, ...rest } = props;
   const { data } = useContext(DataContext);
   return (
     <StandardList
       {...rest}
-      setActions={actionsConfig && renderActions ? (record) => renderActions(record) : undefined}
+      setActions={renderActions ? (record) => renderActions(record) : undefined}
       onChange={handleDataChange}
       data={data}
     />
