b73c29a608bf88b3b30a5f73e1208f83f80e1bea
Yuns Xie <yuns.xie@qq.com>
2019-12-14 12:37:40 +0800

    refactor: use InternalCurd

--- src/DataContext.ts
+++ src/DataContext.ts
@@ -1,10 +1,10 @@
 import React from 'react';
-import Curd from './Curd';
+import { InternalCurd } from './Curd';
 
 export interface DataContextValue<T> {
   modelName?: string;
   data?: { list: T[]; pagination?: any };
-  __curd__?: Curd<T>;
+  __curd__?: InternalCurd<T>;
 }
 
 const DataContext = React.createContext<DataContextValue<any>>({});
