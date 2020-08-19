bc513b9dc57c084e42b5a33e867d089f7060d7be
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-19 15:04:45 +0000

    Deploy theprimone/antd-curd to github.com/theprimone/antd-curd.git:gh-pages

---
+++ main.a71184ba910e882aa7ec.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1415:function(n,o,c){"use strict";c.r(o);c(1416),c(1473)},508:function(n,o,c){c(509),c(657),n.exports=c(1415)},574:function(n,o){}},[[508,1,2]]]);
